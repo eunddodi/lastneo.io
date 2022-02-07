ba4a732bed650b98f895fd4f284959e9f4e92de9
eunddodi <eundippojjack@gmail.com>
2022-02-08 01:38:08 +0900

    add meta tags with react-helmet

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import Register from "./pages/Register";
 import GlobalStyle from "./styles/GlobalStyle";
 import styled, { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-import Nav from "./components/Navbar";
 import NeoHome from "./pages/NeoHome";
 import ResetPw from "./pages/ResetPw";
 import React from "react";
 import NotFound from "./pages/NotFound";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import favicon from "./assets/favicon.ico";
 import mainImg from "./assets/main_web.png";
 
@@ -37,39 +36,45 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Helmet>
-        <title>라스트네오</title>
-        <link rel="icon" href={favicon} />
-        <meta property="og:url" content={currentUrl} />
-        {/* title 정보 */}
-        <meta property="og:title" content="일론 머스크 지수 테스트하기" />
-        {/* 페이지 상세 정보 */}
-        <meta
-          property="og:description"
-          content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
-        />
-        {/* 페이지 대표 이미지 정보 */}
-        <meta property="og:image" content={mainImg} />
-
-        {/* 트위터 메타 정보 */}
-        <meta name="twitter:title" content="일론 머스크 지수 테스트하기" />
-        <meta
-          name="twitter:description"
-          content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
-        />
-        <meta name="twitter:image" content={mainImg} />
-      </Helmet>
       <BrowserRouter>
-        <AppBlock className="app-container">
-          <Switch>
-            <Route exact path="/" component={LandingPage}></Route>
-            <Route exact path="/login" component={EnterId}></Route>
-            <Route path="/register" component={Register}></Route>
-            <Route path="/resetpw" component={ResetPw}></Route>
-            <Route path="/404" component={NotFound}></Route>
-            <Route path="/:id" component={NeoHome}></Route>
-          </Switch>
-        </AppBlock>
+        <HelmetProvider>
+          <Helmet>
+            <title>LASTNEO</title>
+            <meta
+              name="description"
+              content="라스트네오 설명이지롱 App.js안에 있음"
+            />
+            <link rel="icon" href={favicon} />
+            <meta property="og:url" content={currentUrl} />
+            {/* title 정보 */}
+            <meta property="og:title" content="일론 머스크 지수 테스트하기" />
+            {/* 페이지 상세 정보 */}
+            <meta
+              property="og:description"
+              content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+            />
+            {/* 페이지 대표 이미지 정보 */}
+            <meta property="og:image" content={mainImg} />
+
+            {/* 트위터 메타 정보 */}
+            <meta name="twitter:title" content="일론 머스크 지수 테스트하기" />
+            <meta
+              name="twitter:description"
+              content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+            />
+            <meta name="twitter:image" content={mainImg} />
+          </Helmet>
+          <AppBlock className="app-container">
+            <Switch>
+              <Route exact path="/" component={LandingPage}></Route>
+              <Route exact path="/login" component={EnterId}></Route>
+              <Route path="/register" component={Register}></Route>
+              <Route path="/resetpw" component={ResetPw}></Route>
+              <Route path="/404" component={NotFound}></Route>
+              <Route path="/:id" component={NeoHome}></Route>
+            </Switch>
+          </AppBlock>
+        </HelmetProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
